91b5f7273ac84b5ee420483c2e12c5c450b5748f
byemane <beteyemane@hotmail.co.uk>
2019-03-23 22:24:33 +0000

    second project added

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const skillsContainer = document.querySelector('.skills-container')
   const iconsContainer = document.querySelector('.icons-container')
   const icons = document.querySelectorAll('.icons p')
+  const sunsetBarlevardOne = document.querySelector('.sunset-barlevard-one')
+  const sunsetBarlevardTwo = document.querySelector('.sunset-barlevard-two')
+  const sunsetBarlevardThree = document.querySelector('.sunset-barlevard-three')
+  const sunsetBarlevardFour = document.querySelector('.sunset-barlevard-four')
 
+  const currentImage = document.querySelector('#current-image')
+
+  console.log(sunsetBarlevardOne)
+
+  function enlargeImage(image) {
+    const path = './assets/'
+    // image = 'sunset-barlevard.png'
+    const source = path + image
+    currentImage.src = source
+    currentImage.alt = source
+  }
 
   function iconMovement() {
     icons.forEach(icon => {
@@ -20,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   setInterval(iconMovement, 5000)
   iconMovement()
-  
+
   const text = 'BETE YEMANE'
   let i  = 0
   const speed = 200
@@ -53,11 +68,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-
   typewriter()
 
   burger.addEventListener('click', navbarClick)
   skillsArrow.addEventListener('click', skills)
   iconsArrow.addEventListener('click', skills)
+
+  sunsetBarlevardOne.addEventListener('click', enlargeImage('sunset-barlevard.png'))
+  sunsetBarlevardTwo.addEventListener('click', enlargeImage('sunset-barlevard-crawl.png'))
+  sunsetBarlevardThree.addEventListener('click', enlargeImage('sunset-barlevard-home-crawl.png'))
+  sunsetBarlevardFour.addEventListener('click', enlargeImage('sunset-barlevard-add-crawl.png'))
+
 
 })
