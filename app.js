36406c8e780e6905ab0c969c5550a42c2871490f
byemane <beteyemane@hotmail.co.uk>
2019-03-24 22:29:09 +0000

    selected images function works on all thre project images

--- app.js
+++ app.js
@@ -9,16 +9,56 @@ document.addEventListener('DOMContentLoaded', () => {
   const skillsContainer = document.querySelector('.skills-container')
   const iconsContainer = document.querySelector('.icons-container')
   const icons = document.querySelectorAll('.icons p')
-  const images = document.querySelectorAll('.images')
 
-  const currentImage = document.querySelector('#current-image')
+  //sunset barlevard images
+  const currentSbImage = document.querySelector('#current-sb-image')
+  const sbImages = document.querySelectorAll('.sb-images')
 
-  images.forEach(image => {
-    image.addEventListener('click', function(e) {
-      const source = e.target.getAttribute('src')
-      currentImage.src = source
+  //city spot images
+  const currentCsImage = document.querySelector('#current-cs-image')
+  const csImages = document.querySelectorAll('.cs-images')
+
+  //connect 4 images
+  const currentC4Image = document.querySelector('#current-c4-image')
+  const c4Images = document.querySelectorAll('.c4-images')
+
+  const text = 'BETE YEMANE'
+  let i  = 0
+  const speed = 200
+
+  function typewriter() {
+    if (i < text.length) {
+      name.innerHTML += text.charAt(i)
+      i++
+      setTimeout(typewriter, speed)
+    }
+    if (name.innerHTML.length === text.length)
+      arrow.style.display = 'block'
+  }
+
+  //changes src of current image to small image src on click
+  function selectImages(images, currentImage) {
+    images.forEach(image => {
+      image.addEventListener('click', (e) => {
+        const source = e.target.getAttribute('src')
+        currentImage.src = source
+      })
     })
+  }
+
+  //array of all the three project images to re-use the selectImages function
+
+  const dataImages = [
+    [sbImages, currentSbImage],
+    [csImages, currentCsImage],
+    [c4Images, currentC4Image]
+  ]
+
+  dataImages.forEach(dataImage => {
+    selectImages.apply(null, dataImage)
   })
+
+  //generates random pixel place
 
   function iconMovement() {
     icons.forEach(icon => {
@@ -30,26 +70,12 @@ document.addEventListener('DOMContentLoaded', () => {
   setInterval(iconMovement, 5000)
   iconMovement()
 
-  const text = 'BETE YEMANE'
-  let i  = 0
-  const speed = 200
-
   function navbarClick() {
     if(navbar.style.display === 'block') {
       navbar.style.display = 'none'
     } else {
       navbar.style.display = 'block'
     }
-  }
-
-  function typewriter() {
-    if (i < text.length) {
-      name.innerHTML += text.charAt(i)
-      i++
-      setTimeout(typewriter, speed)
-    }
-    if (name.innerHTML.length === text.length)
-      arrow.style.display = 'block'
   }
 
   function skills() {
